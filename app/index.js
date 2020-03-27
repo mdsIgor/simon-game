08715381d5b06b76d8b5f242f8092cf7b3740b2f
mdsigor <igor_melo_silva@hotmail.com>
2020-03-27 15:38:03 -0300

    creating some promises

--- app/index.js
+++ app/index.js
@@ -11,26 +11,72 @@ const myColors = [color1, color2, color3, color4];
 
 
 const getRandonNumber = () => Math.floor(Math.random() * (5 - 1) + 1);
+
+
 const blinkLight = (elem) => {
     
-    const addClass = () => elem.classList.add("simon-color-active");
-    const removeClass = () => elem.classList.remove("simon-color-active");
-
-    addClass();
-    setTimeout(removeClass,1500);
-};
-
-const runGame = () => {
-    modelGame.isStarted = true;
+    console.log(elem);
     
-    const seq = [];
-    seq.push(getRandonNumber());
 
-    seq.forEach(element => {
-        blinkLight(myColors[element]);
+    const addClass = () => elem.classList.add("simon-color-active");
+    const removeClass = () => elem.classList.remove("simon-color-active");    
+
+    return new Promise(resolve => {
+        addClass();
+        setTimeout(() => {
+            removeClass();
+            resolve();
+            
+        },1500);
         
     });
-    //console.log(seq);
+};
+
+const getUserClickedItem = () => {
+    return new Promise( (resolve) => { 
+        
+        color1.addEventListener('click', ()=> resolve(1));
+        color2.addEventListener('click', ()=> resolve(2));
+        color3.addEventListener('click', ()=> resolve(3));
+        color4.addEventListener('click', ()=> resolve(4));
+         
+    });
+};
+
+const runGame = async () => {
+    modelGame.isStarted = true;
+    
+    //creating an array to store game steps
+    const seq = [];
+    const userSteps = [];
+
+    //getting a number between 1 and 4 (-1 in array)
+    for (let index = 0; index < 20; index++) {
+        const sortedNumber = getRandonNumber();    
+        await blinkLight(myColors[sortedNumber-1]);
+        
+        
+        //wait user click
+        const clickedColor = await getUserClickedItem();
+        console.log(clickedColor);        
+        //if they choose the correct one go to the next
+        if(clickedColor == sortedNumber){
+            seq.push(sortedNumber);
+            userSteps.push(clickedColor);
+        }
+        else {
+            console.log("errou");
+            break;
+            
+        }
+
+
+        
+    }
+
+    console.log(seq);
+    
+    
     
 }
 
