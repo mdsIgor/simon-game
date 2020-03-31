d62160696bb9b11e489efdc08d54c4f591db4bc3
mdsigor <igor_melo_silva@hotmail.com>
2020-03-31 12:45:12 -0300

    main funcionality done

--- app/index.js
+++ app/index.js
@@ -1,87 +1,3 @@
-const onoffbutton = document.getElementById('btn-on-off');
-onoffbutton.addEventListener('click', () => {
-    setTimeout(runGame,1000);
-});
-
-const color1 = document.getElementById('color-1');
-const color2 = document.getElementById('color-2');
-const color3 = document.getElementById('color-3');
-const color4 = document.getElementById('color-4');
-const myColors = [color1, color2, color3, color4];
-
-
-const getRandonNumber = () => Math.floor(Math.random() * (5 - 1) + 1);
-
-
-const blinkLight = (elem) => {
-    
-    console.log(elem);
-    
-
-    const addClass = () => elem.classList.add("simon-color-active");
-    const removeClass = () => elem.classList.remove("simon-color-active");    
-
-    return new Promise(resolve => {
-        addClass();
-        setTimeout(() => {
-            removeClass();
-            resolve();
-            
-        },1500);
-        
-    });
-};
-
-const getUserClickedItem = () => {
-    return new Promise( (resolve) => { 
-        
-        color1.addEventListener('click', ()=> resolve(1));
-        color2.addEventListener('click', ()=> resolve(2));
-        color3.addEventListener('click', ()=> resolve(3));
-        color4.addEventListener('click', ()=> resolve(4));
-         
-    });
-};
-
-const runGame = async () => {
-    modelGame.isStarted = true;
-    
-    //creating an array to store game steps
-    const seq = [];
-    const userSteps = [];
-
-    //getting a number between 1 and 4 (-1 in array)
-    for (let index = 0; index < 20; index++) {
-        const sortedNumber = getRandonNumber();    
-        await blinkLight(myColors[sortedNumber-1]);
-        
-        
-        //wait user click
-        const clickedColor = await getUserClickedItem();
-        console.log(clickedColor);        
-        //if they choose the correct one go to the next
-        if(clickedColor == sortedNumber){
-            seq.push(sortedNumber);
-            userSteps.push(clickedColor);
-        }
-        else {
-            console.log("errou");
-            break;
-            
-        }
-
-
-        
-    }
-
-    console.log(seq);
-    
-    
-    
-}
-
-
-
 //model
 const modelGame = {
     _isStarted: false,
@@ -101,5 +17,104 @@ const modelGame = {
 
     set level(level){
         return this._level = level;
+    },
+
+    increaseLevel(){
+        this._level++;
+    }
+}
+
+const onoffbutton = document.getElementById('btn-on-off');
+onoffbutton.addEventListener('click', () => {
+    setTimeout(runGame,1000);
+});
+
+const color1 = document.getElementById('color-1');
+const color2 = document.getElementById('color-2');
+const color3 = document.getElementById('color-3');
+const color4 = document.getElementById('color-4');
+const myColors = [color1, color2, color3, color4];
+
+
+const getRandonNumber = () => Math.floor(Math.random() * (5 - 1) + 1);
+
+
+const blinkLight = (elem) => {
+    
+    //console.log(elem);
+    
+
+    const addClass = () => elem.classList.add("simon-color-active");
+    const removeClass = () => elem.classList.remove("simon-color-active");    
+
+    return new Promise( (resolve) => {
+        addClass();
+        setTimeout(() => {
+            removeClass();
+            resolve();            
+        },1500);
+        
+        
+    });
+};
+
+const getUserClickedItem = () => {
+    return new Promise( resolve => { 
+        
+        color1.addEventListener('click', ()=> resolve(1));
+        color2.addEventListener('click', ()=> resolve(2));
+        color3.addEventListener('click', ()=> resolve(3));
+        color4.addEventListener('click', ()=> resolve(4));
+         
+    });
+};
+
+const blinkAllTillCurrentLevel = async (gameSteps) => {
+    //blink all till current level
+    for (let level = 0; level < gameSteps.length; level++){
+        await blinkLight(myColors[gameSteps[level]-1]);    
+    };
+};
+
+const runGame = async () => {
+    const thisGame = modelGame;
+    thisGame._isStarted = true;
+    thisGame._level = 0;
+    
+    const gameSteps = [];
+    
+    while(thisGame._level < 3){
+     
+        //sorting a number and pushing it to the sequence
+        const sortedNumber = getRandonNumber();
+        gameSteps.push(sortedNumber);
+        //blink all till the current level
+        await blinkAllTillCurrentLevel(gameSteps);
+        
+        //create an array to all user steps every iteration
+        const userSteps = [];
+
+        for (let index = 0; index < gameSteps.length; ) {
+            
+            const currentSelectedColor = await getUserClickedItem();
+            await blinkLight(myColors[currentSelectedColor-1]);
+
+
+            if(gameSteps[index] === currentSelectedColor){
+                console.log("acertou esse botao");
+                userSteps.push(currentSelectedColor);
+                index++;
+            } else {
+                console.log("errou, piscando de novo");
+                index = 0;                
+                await blinkAllTillCurrentLevel(gameSteps);
+            }
+        
+        }
+
+        console.log("saí do bagulho doido");
+        thisGame.increaseLevel();
+        console.log("proximo nivel: " + thisGame._level);
+        
     }
 }
