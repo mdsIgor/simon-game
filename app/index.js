03ded87141d854921b8e31b6651d4abffe8eb9dc
mdsigor <igor_melo_silva@hotmail.com>
2020-03-31 19:45:07 -0300

    strict mode done

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
 const modelGame = {
     _isStarted: false,
     _level: 0,
+    _strictMode: false,
 
     get isStarted(){
         return this._isStarted
@@ -18,6 +19,14 @@ const modelGame = {
     set level(level){
         return this._level = level;
     },
+    
+    get strictMode(){
+        return this._strictMode
+    },
+
+    set strictMode(strictMode){
+        this._strictMode = strictMode;
+    },
 
     increaseLevel(){
         this._level++;
@@ -25,34 +34,68 @@ const modelGame = {
 }
 
 const onoffbutton = document.getElementById('btn-on-off');
-onoffbutton.addEventListener('click', () => {
-    setTimeout(runGame,1000);
-});
-
+const onoffspan = document.getElementById('on-off-span');
 const color1 = document.getElementById('color-1');
 const color2 = document.getElementById('color-2');
 const color3 = document.getElementById('color-3');
 const color4 = document.getElementById('color-4');
 const myColors = [color1, color2, color3, color4];
+const strictModeButton = document.getElementById('btn-strict');
+const startButton = document.getElementById('btn-start');
 
+onoffbutton.addEventListener('click', () => {
+    onoffspan.classList.toggle("simon-btn-on-off-span--active");
+    
+    if(!onoffspan.classList.contains("simon-btn-on-off-span--active")){
+        console.log("desligado");
+    } else {
+        console.log("ligado");
+        prepareGame();
+        
+    }
+    
+      
+});
+
+
+
+
+const prepareGame = () => {
+    const thisGame = modelGame;
+    thisGame._isStarted = true;
+    thisGame._level = 0;
+    
+    strictModeButton.addEventListener("click", ()=>{
+        thisGame._strictMode = true;
+        console.log("strict on");
+    });
+
+
+    startButton.addEventListener("click", () => {
+        setTimeout(()=> {
+            runGame(thisGame);
+        },1000);
+    });
+
+    return thisGame;
+      
+};
 
 const getRandonNumber = () => Math.floor(Math.random() * (5 - 1) + 1);
 
 
 const blinkLight = (elem) => {
-    
-    //console.log(elem);
-    
-
+ 
     const addClass = () => elem.classList.add("simon-color-active");
     const removeClass = () => elem.classList.remove("simon-color-active");    
 
     return new Promise( (resolve) => {
         addClass();
+        elem.children[0].play();
         setTimeout(() => {
-            removeClass();
-            resolve();            
-        },1500);
+            removeClass();      
+        },1000);
+        setTimeout(resolve, 1500);
         
         
     });
@@ -76,14 +119,9 @@ const blinkAllTillCurrentLevel = async (gameSteps) => {
     };
 };
 
-const runGame = async () => {
-    const thisGame = modelGame;
-    thisGame._isStarted = true;
-    thisGame._level = 0;
-    
+const runGame = async (thisGame) => {
     const gameSteps = [];
-    
-    while(thisGame._level < 3){
+    while(thisGame._level < 5){
      
         //sorting a number and pushing it to the sequence
         const sortedNumber = getRandonNumber();
@@ -105,9 +143,17 @@ const runGame = async () => {
                 userSteps.push(currentSelectedColor);
                 index++;
             } else {
-                console.log("errou, piscando de novo");
-                index = 0;                
-                await blinkAllTillCurrentLevel(gameSteps);
+                if(thisGame._strictMode){
+                    console.log("errou no strict mode");
+                    const newGame = prepareGame();
+                    return runGame(newGame);
+                    
+                    
+                } else {
+                    console.log("errou, piscando de novo");
+                    index = 0;                
+                    await blinkAllTillCurrentLevel(gameSteps);
+                }
             }
         
         }
